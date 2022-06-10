a7504579eb2afb216a0db68221af78dc89997f76
krishanu-xc <75176954+krishanu-xc@users.noreply.github.com>
2022-06-11 01:10:52 +0530

    gql to fetch live auctions

--- src/constant/gqls.js
+++ src/constant/gqls.js
@@ -20,25 +20,48 @@ export const GQL_GETMYLISTED = gql`
   }
 `;
 
+// update getlisted to get only listed nfts
+
 export const GQL_GETLISTED = gql`
-query GetListedNft {
-  nfts {
-    id
-    lastListedBy
-    type
-    bids {
+  query GetListedNft {
+    nfts {
       id
-      price
-      address
-      createdAt
+      lastListedBy
+      type
+      bids {
+        id
+        price
+        address
+        createdAt
+      }
+      owner
+      tokenId
+      contract
+      originalPrice
+      auctionDuration
     }
-    owner
-    tokenId
-    contract
-    originalPrice
-    auctionDuration
   }
-}
+`;
+
+export const GQL_GETLIVE = gql`
+  query GetListedNft {
+    nfts(where: { type: 2 }) {
+      id
+      lastListedBy
+      type
+      bids {
+        id
+        price
+        address
+        createdAt
+      }
+      owner
+      tokenId
+      contract
+      originalPrice
+      auctionDuration
+    }
+  }
 `;
 
 export const GQL_GETALL = gql`
